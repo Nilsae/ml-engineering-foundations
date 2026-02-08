8cf92166d62c3fcf961b89f2846c8c1b4b0307c2
Niloo <nilousaeedi@gmail.com>
2026-02-08 11:35:17 -0800

    refinements to hashmap

--- src/mlsys/data_structures/hashmap.py
+++ src/mlsys/data_structures/hashmap.py
@@ -12,54 +12,57 @@ class HashMap:
 
     def set(self, key, value) -> None:
         existed = False
-        bucket_idx = hash(key)%self._capacity
-        for idx, item in enumerate(self._buckets[bucket_idx]):
+        for idx, item in enumerate(self._buckets[self._bucket_index(key)]):
             existing_key, existing_value = item
             if existing_key == key:
-                self._buckets[bucket_idx][idx] = (key, value)
+                self._buckets[self._bucket_index(key)][idx] = (key, value)
                 existed = True
+                break
         if not existed:
-            self._buckets[bucket_idx].append((key, value))
+            self._buckets[self._bucket_index(key)].append((key, value))
             self._size += 1
             if self._size/self._capacity >= self._load_factor:
-                self._buckets.extend([] for _ in range(self._capacity))
-                self._capacity = self._capacity * 2
+                self._resize()
+
 
     def get(self, key, default= _MISSING) -> Any:
-        for existing_key, value in self._buckets[hash(key)%self._capacity]:
+        for existing_key, value in self._buckets[self._bucket_index(key)]:
             if existing_key == key:
                 return value
-        return default
+        if default:
+            return default
+        raise KeyError
 
     def delete(self, key) -> None:
-        deleted = False
-        for existing_key, value in self._buckets[hash(key)%self._capacity]:
+        to_be_deleted = -1
+        for idx, item in enumerate(self._buckets[self._bucket_index(key)]):
+            existing_key, value = item
             if existing_key == key:
-                self._buckets[hash(key)%self._capacity].remove((existing_key, value))
-                deleted = True
-        if deleted:
+                to_be_deleted = idx
+        if to_be_deleted != -1:
+            self._buckets[self._bucket_index(key)].pop(to_be_deleted)
             self._size -= 1
         else:
             raise KeyError
 
     def __contains__(self, key) -> bool:
-        for existing_key, value in self._buckets[hash(key)%self._capacity]:
+        for existing_key, value in self._buckets[self._bucket_index(key)]:
             if existing_key == key:
                 return True
         return False
 
-    # __len__(self) -> int
+    def __len__(self): return self._size
 
     def keys(self) -> list:
-        return [tuple[0] for bucket in self._buckets for tuple in bucket]
+        return [pair[0] for bucket in self._buckets for pair in bucket]
 
         # return self._buckets[:][0]
 
     def values(self) -> list: 
-        return [tuple[1] for bucket in self._buckets for tuple in bucket]
+        return [pair[1] for bucket in self._buckets for pair in bucket]
 
     def items(self) -> list[tuple]:
-        return [tuple for bucket in self._buckets for tuple in bucket]
+        return [pair for bucket in self._buckets for pair in bucket]
 
     def __repr__(self) -> str:
         return f"HashMap(capacity={self._capacity}, buckets={self._buckets})"
@@ -68,19 +71,14 @@ class HashMap:
     # 10. clear(self) -> None
     # 11. __repr__(self) -> str
 
-nil = HashMap(initial_capacity = 8)
-print(nil.keys())
-nil.set(key=10, value=11)
-nil.set(key=67, value=15)
-print(nil.keys())
-print(nil.items())
-print(nil.get(67))
-nil.delete(67)
-nil.set(key=10, value=100)
-nil.set(key=12, value=100)
-nil.set(key=103, value=100)
-nil.set(key=140, value=100)
-nil.set(key=0, value=100)
-nil.set(key=106, value=100)
-nil.set(key=180, value=100)
-print(nil.items())
+    def _bucket_index(self, key):
+        return hash(key)%self._capacity
+
+    def _resize(self):
+        self._capacity = self._capacity * 2
+        extended_buckets = [ [] for _ in range(self._capacity) ]
+        for list in self._buckets:
+            for idx, item in enumerate(list):
+                key, value = item
+                extended_buckets[self._bucket_index(key)].append((key, value))
+        self._buckets = extended_buckets
